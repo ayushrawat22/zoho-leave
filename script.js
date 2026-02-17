33cb3884cb6fe76f678fa43ecb73f7b4e3fb2ae1
ayushrawat22 <ayushrawat271@gmail.com>
2026-02-17 11:12:49 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+const upcomingHolidays = [
+    {
+        date: "04-Mar-2026, Wednesday",
+        name: "Holi",
+        type: "Holiday"
+    },
+    {
+        date: "03-Apr-2026, Friday",
+        name: "Good Friday",
+        type: "Holiday"
+    },
+    {
+        date: "27-May-2026, Wednesday",
+        name: "Eid-Ul-Zuha",
+        type: "Holiday"
+    },
+    {
+        date: "24-Jun-2026, Wednesday",
+        name: "Summer Break - 2026",
+        type: "Holiday"
+    },
+    {
+        date: "25-Jun-2026, Thursday",
+        name: "Summer Break - 2026",
+        type: "Holiday"
+    },
+    {
+        date: "26-Jun-2026, Friday",
+        name: "Summer Break - 2026",
+        type: "Holiday"
+    },
+    {
+        date: "28-Aug-2026, Friday",
+        name: "Raksha Bandhan",
+        type: "Holiday"
+    },
+    {
+        date: "02-Oct-2026, Friday",
+        name: "Gandhi Jayanti",
+        type: "National Holiday"
+    },
+    {
+        date: "20-Oct-2026, Tuesday",
+        name: "Dussehra",
+        type: "Holiday"
+    },
+    {
+        date: "09-Nov-2026, Monday",
+        name: "Diwali",
+        type: "Holiday"
+    },
+    {
+        date: "24-Nov-2026, Tuesday",
+        name: "Gurunanak Jayanti",
+        type: "Holiday"
+    },
+    {
+        date: "25-Dec-2026, Friday",
+        name: "Christmas",
+        type: "Holiday"
+    },
+    {
+        date: "30-Dec-2026, Wednesday",
+        name: "Winter Break - 2026-2027",
+        type: "Holiday"
+    },
+    {
+        date: "31-Dec-2026, Thursday",
+        name: "Winter Break - 2026-2027",
+        type: "Holiday"
+    }
+];
